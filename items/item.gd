325feb92535280d9ed67030d25ea4968697fb460
findemor <contacto@findemor.es>
2024-10-20 16:03:09 +0200

    todos los objetos se recogen

--- items/item.gd
+++ items/item.gd
@@ -1,12 +1,16 @@
+@tool
 class_name Item extends Area2D
 
 enum ItemType { SHIELD, KEY, SWORD }
 
 @export var type:ItemType
-
+@export var texture:CompressedTexture2D:
+	set(value):
+		texture = value
+		$Sprite2D.texture = value
 
 func collect_item():
 	queue_free()
 	
 func get_texture() -> CompressedTexture2D:
-	return $Sprite2D.texture
+	return texture
