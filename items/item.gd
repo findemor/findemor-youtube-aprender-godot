31476cf2b0771f163f91dda555e414285aec05f4
findemor <contacto@findemor.es>
2024-10-20 06:53:56 +0200

    coge objetos

--- items/item.gd
+++ items/item.gd
@@ -3,3 +3,7 @@ class_name Item extends Area2D
 enum ItemType { SHIELD, KEY, SWORD }
 
 @export var type:ItemType
+
+
+func collect_item():
+	queue_free()
--- personaje/inventory.gd
+++ personaje/inventory.gd
@@ -1,6 +1,22 @@
 extends Area2D
 
+## inventario, ej.
+## {
+## 		Shield = 3,
+##		Sword = 1
+## }
+var inventory = {}
 
 func _on_area_entered(area: Area2D) -> void:
 	print(area.name)
-	area.queue_free()
+	if area is Item:
+		add_item_to_inventory(area)
+		area.collect_item()
+		
+func add_item_to_inventory(item:Item):
+	if not inventory.has(item.type):
+		inventory[item.type] = 1
+	else:
+		inventory[item.type] = inventory[item.type] + 1
+	prints("añadido al inventario objeto de tipo", Item.ItemType.keys()[item.type], "ahora tenemos:", inventory[item.type])
+		
